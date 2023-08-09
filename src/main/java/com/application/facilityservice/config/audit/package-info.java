f5b60fb56afd66ff77419c9cd1e11a4816c02b76
Sai Sunhith Reddy Nagireddy <sxn220005@utdallas.edu>
2023-08-08 19:06:30 -0500

    Initial version of facilityservice generated by JHipster-6.4.0

---
+++ src/main/java/com/application/facilityservice/config/audit/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Audit specific code.
+ */
+package com.application.facilityservice.config.audit;
---
+++ src/main/java/com/application/facilityservice/web/rest/vm/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * View Models used by Spring MVC REST controllers.
+ */
+package com.application.facilityservice.web.rest.vm;
